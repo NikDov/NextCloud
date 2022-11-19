d3da736d3247536e020da7b6db300dafb4f6e030
DevFedOps <mac-air@mail.ru>
2022-11-19 14:17:25 +0300

    update file

--- nextcloud.py
+++ nextcloud.py
@@ -5,13 +5,12 @@ nc.login('admin', '1996pd16')
 
 def create_user(user_name, password):
     global nc
-    nc.create_user(user_name, password)
-    print("<>INFO<>Creating user:", user_name)
-    
     try:
-        print(nc.get_user(user_name)["displayname"])
+        nc.create_user(user_name, password)
+        print("<>INFO<>Creating user:", user_name)
+        print("<>INFO<>Get user displayname:", nc.get_user(user_name)["displayname"])
         print("<>INFO<>User created")
     except Exception:
-        print("Can not found user:", user_name)
+        print("<>ERROR<>Can not create user:", user_name)
 
-create_user("Tom", "10021998@@@")
+create_user("Fedor.Chesnokov", "10021998@@@")
