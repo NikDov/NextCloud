08bfb47c9cb02db5bb9e7ca3f89630ec0399cb59
DevFedOps <mac-air@mail.ru>
2022-11-19 14:10:14 +0300

    update file

--- nextcloud.py
+++ nextcloud.py
@@ -1,7 +1,17 @@
 import nextcloud_client
 
 nc = nextcloud_client.Client('http://nextcloud.pd16.com:8081')
-
 nc.login('admin', '1996pd16')
 
-nc.get_users()
+def create_user(user_name, password):
+    global nc
+    nc.create_user(user_name, password)
+    print("<>INFO<>Creating user:", user_name)
+    
+    try:
+        print(nc.get_user(user_name)["displayname"])
+        print("<>INFO<>User created")
+    except Exception:
+        print("Can not found user:", user_name)
+
+create_user("Tom", "10021998@@@")
