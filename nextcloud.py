b3607936d6a87c71e1a0cf6f9b98c88e10f5b5b5
DevFedOps <mac-air@mail.ru>
2022-11-19 14:44:37 +0300

    update file

--- nextcloud.py
+++ nextcloud.py
@@ -17,3 +17,17 @@ def create_user(user_name, password):
         print("<>INFO<>User created")
     except Exception:
         print("<>ERROR<>Can not create user:", user_name)
+
+# на вход функция принимает два аргумента: user_name и group_name
+# user_name - это имя пользователя в системе nextcloud
+# group_name - это имя группы в системе nextcloud
+# например, add_user_to_group("Ivanov.Ivan.Ivanovich", "gold")
+
+def add_user_to_group(user_name, group_name):
+    global nc
+    try:
+        nc.add_user_to_group(user_name, group_name)
+        print("<>INFO<>Adding user:", user_name, "to group:", group_name)
+        print("<>INFO<>User added to group")
+    except Exception:
+        print("<>ERROR<>Can not add user to group:", group_name)
