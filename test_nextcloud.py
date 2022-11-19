c210be58a430a3e00cfce5d8ffcaf674f0ef18f2
NikDov <kooolaan@gmail.com>
2022-11-19 12:55:58 +0300

    api nc.py

---
+++ test_nextcloud.py
@@ -0,0 +1,16 @@
+import nextcloud_client
+
+nc = nextcloud_client.Client('http://nextcloud.pd16.com:8081')
+
+nc.login('admin', '1996pd16')
+
+# users = nc.get_users()
+# for user in users:
+#     print("hello,", (user))
+
+# просмотр методов
+#print(dir(nc))
+
+#nc.create_user('Maria', '120612PdMsk')
+
+print('nc.get_users')
